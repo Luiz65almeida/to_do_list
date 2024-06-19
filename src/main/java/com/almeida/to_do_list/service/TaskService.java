e66f9f7f3e8e8cd74395e39cd8e5997f37afdc15
Luiz Henrique Almeida <luizhenrique65almeida@gmail.com>
2024-06-19 09:50:31 -0300

    Criação da classe TaskService e as recras de negócio do CRUD

---
+++ src/main/java/com/almeida/to_do_list/service/TaskService.java
@@ -0,0 +1,45 @@
+package com.almeida.to_do_list.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.almeida.to_do_list.model.Task;
+import com.almeida.to_do_list.repository.TaskRepository;
+
+@Service
+public class TaskService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public List<Task> findAll() {
+        return taskRepository.findAll();
+    }
+
+    public Task findById(Long id) {
+        Optional<Task> obj = taskRepository.findById(id);
+        return obj.get();
+    }
+
+    public Task insert(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public void delete(Long id) {
+        taskRepository.deleteById(id);
+    }
+
+    public Task update(Long id, Task obj) {
+        Task entity = taskRepository.getReferenceById(id);
+        updateData(entity, obj);
+        return taskRepository.save(entity);
+    }
+
+    private void updateData(Task entity, Task obj) {
+        entity.setName(obj.getName());
+    }
+
+}
