7e01b38bb82cce36d67e68514a63069e4e0f24b7
Luiz Henrique Almeida <luizhenrique65almeida@gmail.com>
2024-06-20 10:26:16 -0300

    Iniciando a implentação do DTO

---
+++ src/main/java/com/almeida/to_do_list/dto/DtoTask.java
@@ -0,0 +1,46 @@
+package com.almeida.to_do_list.dto;
+
+import com.almeida.to_do_list.common.enuns.PriorityLevelEnum;
+
+public class DtoTask {
+
+    String name;
+
+    String description;
+
+    boolean priority;
+
+    public PriorityLevelEnum priorityLevel;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isPriority() {
+        return priority;
+    }
+
+    public void setPriority(boolean priority) {
+        this.priority = priority;
+    }
+
+    public PriorityLevelEnum getPriorityLevel() {
+        return priorityLevel;
+    }
+
+    public void setPriorityLevel(PriorityLevelEnum priorityLevel) {
+        this.priorityLevel = priorityLevel;
+    }
+}
