bf03680028b0defbcf8e05b3f3001f5ed6d8c63d
Luiz Henrique Almeida <luizhenrique65almeida@gmail.com>
2024-06-21 18:11:42 -0300

    implementado as variáveis da classe User

--- src/main/java/com/almeida/to_do_list/model/User.java
+++ src/main/java/com/almeida/to_do_list/model/User.java
@@ -1,5 +1,88 @@
 package com.almeida.to_do_list.model;
 
+import java.util.Date;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
 public class User {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String name;
+
+  private String email;
+
+  private String password;
+
+  private Date dataCadastro;
+
+  @OneToMany(mappedBy = "")
+  private List<Task> tasks;
+
+  public User() {
+  }
+
+  public User(String name, String email, String password, Date dataCadastro, List<Task> tasks) {
+    this.name = name;
+    this.email = email;
+    this.password = password;
+    this.dataCadastro = dataCadastro;
+    this.tasks = tasks;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public Date getDataCadastro() {
+    return dataCadastro;
+  }
+
+  public void setDataCadastro(Date dataCadastro) {
+    this.dataCadastro = dataCadastro;
+  }
+
+  public List<Task> getTasks() {
+    return tasks;
+  }
+
+  public void setTasks(List<Task> tasks) {
+    this.tasks = tasks;
+  }
 }
